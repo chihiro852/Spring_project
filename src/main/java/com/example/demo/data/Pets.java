bb64dcab3ec3c5680ed9bf78e47ecbc6ffd07ba0
chihiro <seassist.nchihiro@gmail.com>
2023-05-15 14:35:33 +0900

    create API

--- src/main/java/com/example/demo/data/Pets.java
+++ src/main/java/com/example/demo/data/Pets.java
@@ -1,11 +1,10 @@
 package com.example.demo.data;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Data;
+
+@Data
 public class Pets {
 
 	@JsonProperty("id")
@@ -15,39 +14,5 @@ public class Pets {
 	private String type;
 
 	@JsonProperty("price")
-	private int price;
-
-	@JsonIgnore
-	private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
-
-	@JsonProperty("id")
-	public int getId() {
-		return id;
-	}
-
-	@JsonProperty("id")
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@JsonProperty("type")
-	public String getType() {
-		return type;
-	}
-
-	@JsonProperty("type")
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	@JsonProperty("price")
-	public int getPrice() {
-		return price;
-	}
-
-	@JsonProperty("price")
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
+	private double price;
 }
