f0c7f3f225bb715d434283c61618285620d6120c
chihiro <seassist.nchihiro@gmail.com>
2023-06-12 18:41:55 +0900

    AttendanceAndAbsence add.

---
+++ src/main/java/com/example/demo/attendanceAndAbsence/controller/GetEmployee.java
@@ -0,0 +1,8 @@
+package com.example.demo.attendanceAndAbsence.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GetEmployee {
+
+}
---
+++ src/main/java/com/example/demo/attendanceAndAbsence/data/Employee.java
@@ -0,0 +1,31 @@
+package com.example.demo.attendanceAndAbsence.data;
+
+import java.sql.Timestamp;
+import java.time.YearMonth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+	@JsonProperty("id")
+	private int id;
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("hometown")
+	private String hometown;
+
+	@JsonProperty("joining_month")
+	private YearMonth joining_month;
+
+	@JsonProperty("created_at")
+	private Timestamp created_at;
+
+	@JsonProperty("updated_at")
+	private Timestamp updated_at;
+
+}
